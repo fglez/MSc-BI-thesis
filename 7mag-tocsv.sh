155205bc93af09a56225493213282c90b5281d46
fglez <80017003+fglez@users.noreply.github.com>
2021-11-08 23:43:07 -0600

    Update 7mag-tocsv.sh
    Changes to make it more general

--- 7mag-tocsv.sh
+++ 7mag-tocsv.sh
@@ -2,7 +2,7 @@
 
 prefix=$1
 
-scp mcontreras@148.247.230.5:/LUSTRE/usuario/mcontreras/thesisseq/cult2021/${prefix}/checkm/storage/bin_stats_ext.tsv .
+scp user@domain:/path/to/checkm/storage/bin_stats_ext.tsv .
 
 cp bin_stats_ext.tsv ${prefix}checkm.csv #make a copy of the checkm report to reformat it to readable csv file
 
@@ -16,4 +16,4 @@ sed -i 's/}//g' ${prefix}checkm.csv
 
 cut -d, -f1,12,13 ${prefix}checkm.csv > small${prefix}check.csv #make a small version with only bin name, completeness and contamination
 
-rm ${prefix}checkm.csv #if you want to remove the big csv file remove the hashtag
+#rm ${prefix}checkm.csv #if you want to remove the big csv file remove the hashtag
